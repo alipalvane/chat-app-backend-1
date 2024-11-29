cf022a4d69deb7e45b674360adc050bf20ce492d
Ali <alipalvane@gmail.com>
2024-11-29 14:07:33 +0330

    finish backend

--- server/controllers/ChannelController.js
+++ server/controllers/ChannelController.js
@@ -47,3 +47,25 @@ export const getUserChannels = async (req, res, next) => {
     return res.status(500).send("internal error from server");
   }
 };
+
+export const getChannelMessages = async (req, res, next) => {
+  try {
+    const {channelId} = req.params
+    const channel = await Channel.findById(channelId).populate({
+      path: "messages",
+      populate: {
+        path: "sender",
+        select: "firstName lastName image color",
+      }
+    })
+    if(!channel){
+      return res.status(404).send("channel not found")
+    }
+    const messages = channel.messages
+    return res.status(201).json({ messages });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("internal error from server");
+  }
+};
+
--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,6 @@
 import { Server as SocketIOServer } from "socket.io";
 import Message from "./models/MessagesModel.js";
+import Channel from "./models/ChannelModel.js";
 
 const setupSocket = (server) => {
   const io = new SocketIOServer(server, {
@@ -39,6 +40,43 @@ const setupSocket = (server) => {
     }
   };
 
+  const sendChannelMessage = async (message) => {
+    const { channelId, sender, content, messageType, fileUrl } = message;
+    const createdMessage = await Message.create({
+      sender,
+      recipient: null,
+      content,
+      messageType,
+      timestamp: new Date(),
+      fileUrl,
+    });
+
+    const messageData = await Message.findById(createdMessage._id)
+      .populate("sendrer", "id email firstName lastName image color")
+      .exec();
+
+    await Channel.findByIdAndUpdate(channelId, {
+      $push: { messages: createdMessage._id },
+    });
+
+    const channel = await Channel.findById(channelId).populate("members");
+
+    const finalData = { ...messageData._doc, channelId: channel._id };
+
+    if (channel && channel.members) {
+      channel.members.forEach((member) => {
+        const memberSocketId = userSocketMap.get(member._id.toString());
+        if (memberSocketId) {
+          io.to(memberSocketId).emit("recieveChannelMessage", finalData);
+        }
+      });
+      const AdminSocketId = userSocketMap.get(channel.admin._id.toString());
+      if (AdminSocketId) {
+        io.to(AdminSocketId).emit("recieveChannelMessage", finalData);
+      }
+    }
+  };
+
   io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
 
@@ -48,6 +86,7 @@ const setupSocket = (server) => {
       console.log("user id not provided during connection");
     }
     socket.on("sendMessage", sendMessage);
+    socket.on("send-channel-message", sendChannelMessage);
     socket.on("disconnect", () => disconnect(socket));
   });
 };
